2f61b63d64e4588569bd01519eb682bdd75ea1f5
lluke-chen <lukechen67@gmail.com>
2023-05-25 13:35:20 +0200

    Minor edit

--- js/requests-handler.js
+++ js/requests-handler.js
@@ -1,7 +1,7 @@
 var url = 'https://api-dot-valued-etching-380113.ew.r.appspot.com/';
 
 function generateNumbers() {
-    for (var i = 0; i < 10000; i++) {
+    for (var i = 0; i < 5000; i++) {
         fetch(url + 'generate-numbers', {
             method: 'GET',
             mode: 'no-cors',
@@ -17,12 +17,12 @@ function getResults() {
         method: 'GET',
         mode: 'no-cors',
     })
-        .then((response) => response.text())
+        .then((response) => response.json())
         .then((html) => {
             var resultsElement = document.getElementById('results');
+            console.log(html);
             resultsElement.innerHTML = html;
+            console.log(response.text());
         })
         .catch((error) => {});
-
-    console.log('Results fetched');
 }
