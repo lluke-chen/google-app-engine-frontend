e60a553134fe371a6a9bcc7fe1f1ece7cdca91e0
lluke-chen <lukechen67@gmail.com>
2023-05-25 14:34:50 +0200

    Added sleep func

--- js/requests-handler.js
+++ js/requests-handler.js
@@ -1,15 +1,31 @@
 var url = 'https://api-dot-valued-etching-380113.ew.r.appspot.com/';
 
-function generateNumbers() {
-    for (var i = 0; i < 1000; i++) {
-        fetch(url + 'generate-numbers', {
-            method: 'GET',
-            mode: 'no-cors',
-        })
-            .then((response) => {})
-            .catch((error) => {});
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function generateNumbers() {
+    for (var i = 0; i < 10; i++) {
+        // Generate 1000 numbers
+        console.log('Generating...');
+        for (var i = 0; i < 1000; i++) {
+            fetch(url + 'generate-numbers', {
+                method: 'GET',
+                mode: 'no-cors',
+            })
+                .then((response) => {})
+                .catch((error) => {});
+        }
+
+        for (let i = 0; i < 25; i++) {
+            console.log(`Waiting ${i} seconds...`);
+            await sleep(i * 1000);
+        }
+
+        console.log('Done.');
+
+        // Sleep function
     }
-    console.log('Numbers generated');
 }
 
 // function getResults() {
